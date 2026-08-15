668fac7758a01d07cc96f55890f87408c10aec0f
tmr <tmr@tmr.tmr>
2026-08-15 11:42:26 -0400

    first commmit

---
+++ ans.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define MAXN 1000000
+#define fi first
+#define se second
+#define print(v)                                                               \
+  for (auto &x : v)                                                            \
+    cout << x << ' ';                                                          \
+  cout << '\n'
+
+void solve() {}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int tt = 1;
+  // cin >> tt;
+  while (tt--)
+    solve();
+}
