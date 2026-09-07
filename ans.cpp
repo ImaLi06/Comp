6c7790dc4840fd4cc4e025d6d0d768d1b90e3d68
ImaLi06 <imanollimache@gmail.com>
2026-09-07 14:44:33 -0400

    add kruskal

--- ans.cpp
+++ ans.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-#define MAXN 1000000
+#define MAXN 200000
 #define MOD 1000000007
 #define INF 1e9
 #define fi first
@@ -17,7 +17,90 @@ typedef long long ll;
     cout << '\n';                                                              \
   }
 
-void solve() {}
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  vector<pair<int, int>> b;
+  string ans = "";
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    b.push_back({a[i], i});
+    ans += '1';
+  }
+
+  sort(b.rbegin(), b.rend());
+  if (b[0].fi == -1) {
+    for (int i = 0; i < n - 1; i++)
+      cout << 0;
+    cout << 1 << "\n";
+    return;
+  }
+
+  int acn = 1;
+  for (int i = 1; i < n; i++) {
+    if (a[i] == a[i - 1] && a[i] > 0)
+      acn++;
+    else
+      acn = 1;
+
+    if (acn > 2) {
+      cout << "-1\n";
+      return;
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    int ind = b[i].se;
+    if (ans[ind] == '0' || a[ind] <= 0)
+      continue;
+
+    int co = b[i].fi;
+    if (ind == n - 1 || (ind < n - 1 && a[ind] != a[ind + 1])) {
+      for (int i = ind; i < n && co >= 0; i++, co--) {
+        if ((a[i] != co && a[i] != -1) || ans[i] == '0' ||
+            (co == 0 && a[i] > 0)) {
+          cout << "-1\n";
+          return;
+        }
+        if (co > 0)
+          ans[i] = '0';
+      }
+    }
+
+    co = b[i].fi;
+    if (ind == 0 || (ind > 0 && a[ind] != a[ind - 1])) {
+      for (int i = ind; i >= 0 && co >= 0; i--, co--) {
+        if ((a[i] != co && a[i] != -1) || (ans[i] == '0' && i != ind) ||
+            (co == 0 && a[i] > 0)) {
+          cout << "-1\n";
+          return;
+        }
+        if (co > 0)
+          ans[i] = '0';
+      }
+    }
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (a[i] == -1)
+      continue;
+    bool l = false;
+    bool r = false;
+
+    if (i - a[i] >= 0 && ans[i - a[i]] == '1')
+      l = true;
+    if (i + a[i] < n && ans[i + a[i]] == '1')
+      r = true;
+
+    if (!l && !r) {
+      cout << -1 << "\n";
+      return;
+    }
+  }
+
+  cout << ans << "\n";
+}
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
