9824868898a94a52875a2176e939c8ec776fe0c3
ImaLi06 <imanollimache@gmail.com>
2026-09-12 12:02:48 -0400

    add segment tree lazy

---
+++ algo/data_structures/segment_tree_lazy.cpp
@@ -0,0 +1,61 @@
+typedef ll tn;  // segment-tree node type
+typedef ll tl;  // lazy type
+#define NEUT 0  // operation neutral
+#define CLEAR 0 // cleared lazy node
+tn oper(tn a, tn b) { return a + b; }
+void acum(tl &a, tl v) { a += v; }
+tn calc(int s, int e, tn a, tl v) {
+  a += (e - s) * v;
+  return a;
+}
+struct STree {
+  vector<tn> st;
+  vector<tl> lazy;
+  int n;
+  STree(int n) : st(4 * n + 5, NEUT), lazy(4 * n + 5, CLEAR), n(n) {}
+  void init(int k, int s, int e, vector<tn> &a) {
+    if (e - s == 1)
+      st[k] = a[s];
+    else {
+      int m = (s + e) / 2;
+      init(2 * k, s, m, a);
+      init(2 * k + 1, m, e, a);
+      st[k] = oper(st[2 * k], st[2 * k + 1]);
+    }
+  }
+  void push(int k, int s, int e) {
+    if (lazy[k] == CLEAR)
+      return;
+    st[k] = calc(s, e, st[k], lazy[k]);
+    if (e - s != 1) {
+      acum(lazy[2 * k], lazy[k]);
+      acum(lazy[2 * k + 1], lazy[k]);
+    }
+    lazy[k] = CLEAR;
+  }
+  void upd(int k, int s, int e, int a, int b, tl v) {
+    push(k, s, e);
+    if (e <= a || b <= s)
+      return;
+    if (a <= s && e <= b) {
+      acum(lazy[k], v);
+      push(k, s, e);
+      return;
+    }
+    int m = (s + e) / 2;
+    upd(2 * k, s, m, a, b, v), upd(2 * k + 1, m, e, a, b, v);
+    st[k] = oper(st[2 * k], st[2 * k + 1]);
+  }
+  tn query(int k, int s, int e, int a, int b) {
+    if (e <= a || b <= s)
+      return NEUT;
+    push(k, s, e);
+    if (a <= s && e <= b)
+      return st[k];
+    int m = (s + e) / 2;
+    return oper(query(2 * k, s, m, a, b), query(2 * k + 1, m, e, a, b));
+  }
+  void upd(int a, int b, tl v) { upd(1, 0, n, a, b, v); }
+  tn query(int a, int b) { return query(1, 0, n, a, b); }
+  void init(vector<tn> &a) { init(1, 0, n, a); }
+};
