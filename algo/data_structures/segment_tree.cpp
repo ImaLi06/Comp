89fe7cea37b2db126a1e81a2f6490061aba5e9b0
ImaLi06 <imanollimache@gmail.com>
2026-09-16 12:00:50 -0500

    update

--- algo/data_structures/segment_tree.cpp
+++ algo/data_structures/segment_tree.cpp
@@ -2,9 +2,7 @@ template <typename T, T (*oper)(T, T)> struct STree {
   vector<T> st;
   int n;
   T NEUT;
-
   STree(int n, T neut) : st(4 * n + 5, neut), n(n), NEUT(neut) {}
-
   void init(int k, int s, int e, T *a) {
     if (s + 1 == e) {
       st[k] = a[s];
@@ -15,7 +13,6 @@ template <typename T, T (*oper)(T, T)> struct STree {
     init(2 * k + 1, m, e, a);
     st[k] = oper(st[2 * k], st[2 * k + 1]);
   }
-
   void upd(int k, int s, int e, int p, T v) {
     if (s + 1 == e) {
       st[k] = v;
@@ -28,7 +25,6 @@ template <typename T, T (*oper)(T, T)> struct STree {
       upd(2 * k + 1, m, e, p, v);
     st[k] = oper(st[2 * k], st[2 * k + 1]);
   }
-
   T query(int k, int s, int e, int a, int b) {
     if (s >= b || e <= a)
       return NEUT;
@@ -37,7 +33,6 @@ template <typename T, T (*oper)(T, T)> struct STree {
     int m = (s + e) / 2;
     return oper(query(2 * k, s, m, a, b), query(2 * k + 1, m, e, a, b));
   }
-
   void init(T *a) { init(1, 0, n, a); }
   void upd(int p, T v) { upd(1, 0, n, p, v); }
   T query(int a, int b) { return query(1, 0, n, a, b); }
--- algo/maths/spf_sieve.cpp
+++ algo/maths/spf_sieve.cpp
@@ -1,10 +1,8 @@
 const int SIEVE_MAX = 1000000;
-
 int spf[SIEVE_MAX + 1];
 void sieve() {
   for (int i = 1; i <= SIEVE_MAX; ++i)
     spf[i] = i;
-
   for (int i = 2; i * i <= SIEVE_MAX; ++i) {
     if (spf[i] == i) {
       for (int j = i * i; j <= SIEVE_MAX; j += i) {
