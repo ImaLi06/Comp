d95be7fdcde4083bef9d2ae8f248b6fca33decd1
ImaLi06 <imanollimache@gmail.com>
2026-09-21 23:52:37 -0500

    add fenwick tree

--- ans.cpp
+++ ans.cpp
@@ -16,23 +16,133 @@ typedef long long ll;
       cout << x << ' ';                                                        \
     cout << '\n';                                                              \
   }
-int freq[MAXN + 1];
-void solve() {
+struct Node {
+  ll cval;
+  ll oval;
+};
+struct LNode {
+  ll t;
+  ll val;
+};
+typedef Node tn;     // segment-tree node type
+typedef LNode tl;    // lazy type
+#define NEUT {0, 0}  // operation neutral
+#define CLEAR {0, 0} // cleared lazy node
+tn oper(tn a, tn b) { return {a.cval + b.cval, a.oval + b.oval}; }
+void acum(tl &a, tl v) { a = v; }
+tn calc(int s, int e, tn a, tl v) {
+  if (v.t == 1)
+    a.cval = 0;
+  else if (v.t == 2)
+    a.cval = a.oval;
+  else if (v.t == 3)
+    a.cval = v.val;
+  return a;
+}
+struct STree {
+  vector<tn> st;
+  vector<tl> lazy;
   int n;
-  cin >> n;
+  STree(int n) : st(4 * n + 5, NEUT), lazy(4 * n + 5, CLEAR), n(n) {}
+  void init(int k, int s, int e, vector<tn> &a) {
+    if (e - s == 1)
+      st[k] = a[s];
+    else {
+      int m = (s + e) / 2;
+      init(2 * k, s, m, a);
+      init(2 * k + 1, m, e, a);
+      st[k] = oper(st[2 * k], st[2 * k + 1]);
+    }
+  }
+  void push(int k, int s, int e) {
+    if (lazy[k].t == 0)
+      return;
+    st[k] = calc(s, e, st[k], lazy[k]);
+    if (e - s != 1) {
+      acum(lazy[2 * k], lazy[k]);
+      acum(lazy[2 * k + 1], lazy[k]);
+    }
+    lazy[k] = CLEAR;
+  }
+  void upd(int k, int s, int e, int a, int b, tl v) {
+    push(k, s, e);
+    if (e <= a || b <= s)
+      return;
+    if (a <= s && e <= b) {
+      acum(lazy[k], v);
+      push(k, s, e);
+      return;
+    }
+    int m = (s + e) / 2;
+    upd(2 * k, s, m, a, b, v), upd(2 * k + 1, m, e, a, b, v);
+    st[k] = oper(st[2 * k], st[2 * k + 1]);
+  }
+  tn query(int k, int s, int e, int a, int b) {
+    if (e <= a || b <= s)
+      return NEUT;
+    push(k, s, e);
+    if (a <= s && e <= b)
+      return st[k];
+    int m = (s + e) / 2;
+    return oper(query(2 * k, s, m, a, b), query(2 * k + 1, m, e, a, b));
+  }
+  int search(int k, int s, int e, ll acc, ll obj) {
+    if (s + 1 == e)
+      return s;
+    push(k, s, e);
+    int m = (s + e) / 2;
+    push(k * 2, s, m);
+    if (acc + st[k * 2].cval > obj) {
+      return search(k * 2, s, m, acc, obj);
+    }
+    push(k * 2 + 1, m, e);
+    return search(k * 2 + 1, m, e, acc + st[k * 2].cval, obj);
+  }
+  void upd(int a, int b, tl v) { upd(1, 0, n, a, b, v); }
+  tn query(int a, int b) { return query(1, 0, n, a, b); }
+  void init(vector<tn> &a) { init(1, 0, n, a); }
+  int search(ll obj) { return search(1, 0, n, 0, obj); }
+};
+void solve() {
+  ll n, m;
+  cin >> n >> m;
   vector<ll> a(n);
   for (int i = 0; i < n; i++)
     cin >> a[i];
+  vector<Node> base;
   for (int i = 0; i < n; i++)
-    freq[a[i]]++;
-  for (int i = MAXN; i >= 1; i--) {
-    int cnt = 0;
-    for (int j = i; j <= MAXN; j += i) {
-      cnt += freq[j];
-      if (cnt > 1) {
-        cout << i;
-        return;
+    base.push_back({a[i], a[i]});
+  vector<ll> pref = a;
+  for (int i = 1; i < n; i++)
+    pref[i] = pref[i - 1] + a[i];
+  STree st(n);
+  st.init(base);
+  while (m--) {
+    int t;
+    cin >> t;
+    if (t == 1) {
+      ll b, v;
+      cin >> b >> v;
+      Node sum = st.query(0, b);
+      int limit = st.search(sum.cval - v);
+      if (sum.cval - v < 0) {
+        st.upd(0, b, {1, 0});
+        continue;
       }
+      Node subsum = {0, 0};
+      if (limit + 1 < b) {
+        subsum = st.query(limit + 1, b);
+        st.upd(limit + 1, b, {1, 0});
+      }
+      ll nval = st.query(limit, limit + 1).cval - v + subsum.cval;
+      st.upd(limit, limit + 1, {3, nval});
+    } else {
+      int l, r;
+      cin >> l >> r;
+      ll summ = st.query(l - 1, r).cval;
+      ll oris = pref[r - 1] - (l - 2 >= 0 ? pref[l - 2] : 0);
+      cout << oris - summ << "\n";
+      st.upd(l - 1, r, {2, 0});
     }
   }
 }
